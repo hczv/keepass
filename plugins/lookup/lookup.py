085a1f3fa658375e987db20443d09b2627bd9dcb
hczv <hczv@hotmail.dk>
2024-07-28 19:19:27 +0200

    made it less painful

--- plugins/lookup/lookup.py
+++ plugins/lookup/lookup.py
@@ -32,34 +32,52 @@ def find_entries_with_field(kp, field_name, field_value):
     for entry in kp.entries:
         # Check if the entry has the specific field and if its value matches
         if field_name in entry.custom_properties and field_value in entry.custom_properties[field_name]:
+            # Handle multiple lines in the field
             for entryVal in entry.custom_properties[field_name].split():
                 if entryVal == field_value:
                     matching_entries.append(entry)
 
     return matching_entries
 
+
+def find_entry_with_field(self, field_name, field_value, display):
+    matching_entries = []
+
+    for entry in self.kp.entries:
+        if field_name in entry.custom_properties and field_value in entry.custom_properties[field_name]:
+            for entryVal in entry.custom_properties[field_name].split():
+                if entryVal == field_value:
+                    matching_entries.append(entry)
+
+    if len(matching_entries) > 1:
+        display.error("More than one entry has the field %s matching: %s" % (field_name, field_value))
+        for matching_entry in matching_entries:
+            display.error("%s: %s" % (matching_entry.title, matching_entry.path))
+        raise AnsibleError("Too many matches for %s", field_name)
+    elif len(matching_entries) == 1:
+        return matching_entries[0]
+
+    return None
+
 class LookupModule(LookupBase):
   kp = None
 
   def run(self, terms, variables=None, **kwargs):
-    #self.set_options(var_options=variables, direct=kwargs)
-    #default = self.get_option('default')
+
+    # Require username or password term in the lookup
+    if terms[0] != "username" and terms[0] != "password":
+        raise AnsibleError("KeePass: term username or password is required.")
+
+    runtype = terms[0]
+
     vars = getattr(self._templar, '_available_variables', {})
     host = vars['inventory_hostname']
-
-    runtype = None
-
-    if terms[0] == "username":
-        runtype = "username"
-    elif terms[0] == "password":
-        runtype = "password"
-    else:
-        raise AnsibleError("KeePass: term username or password is required.")
 
     # Check if there is a second value, and it's actually has someting, assumts it's a new hostname to pull credentials for.
     if len(terms) == 2 and terms[1]:
         host = terms[1]
         display.v("Keepass Host overwritten to: " + host)
+
 
     cache_identifier = host + "|" + runtype
     cached_data = self._get_cached_data(variables, cache_identifier)
@@ -67,8 +85,6 @@ class LookupModule(LookupBase):
     if cached_data is not None:
         display.debug("Using cached %s entry for %s" % (runtype, host))
         return [cached_data]
-
-
 
     # Path to your KeePass database file
     kdbx_file = vars['keepass_dbx']
@@ -83,36 +99,24 @@ class LookupModule(LookupBase):
 
     domain = re.sub(r'^[^.]+\.', '', host)
 
-    result = ['']
-    domain_results = find_entries_with_field(self.kp, domain_field, domain)
-    if len(domain_results) > 1:
-       display.error("More than one entry has the field %s matching: %s" % (domain_field, domain))
-       for domain_result in domain_results:
-           display.error("%s: %s" % (domain_result.title, domain_result.path))
-       raise AnsibleError("Too many matches for %s", domain_field)
-    elif len(domain_results) == 1:
-        result[0] = domain_results[0]
+
+    result = find_entry_with_field(self, host_field, host, display)
+    if result is None:
+        result = find_entry_with_field(self, domain_field, domain, display)
+
+    if result is None:
+        raise AnsibleError("KeePass: Could not locate credentials for %s" % host)
+
+    # Set cache
+    self._set_cached_data(variables, cache_identifier, self._get_result_property(runtype, result))
+    return [self._get_result_property(runtype, result)]
 
 
-    host_results = find_entries_with_field(self.kp, host_field, host)
-    if len(host_results) > 1:
-       display.error("More than one entry has the field %s matching: %s" % (host_field, host))
-       for host_result in host_results:
-           display.error("%s: %s" % (host_result.title, host_result.path))
-       raise AnsibleError("Too many matches for %s", host_field)
-    elif len(host_results) == 1:
-        result[0] = host_results[0]
-
-    #print("Retrieved %s entry for %s" % (runtype, host))
-
-    if runtype == "username":
-        self._set_cached_data(variables, cache_identifier, result[0].username)
-        return [result[0].username]
-    elif runtype == "password":
-        self._set_cached_data(variables, cache_identifier, result[0].password)
-        return [result[0].password]
-
-    #return [host_results[0].password]
+  def _get_result_property(self, runtype, result):
+        if runtype == "username":
+            return result.username
+        elif runtype == "password":
+            return result.password
 
   def _get_cached_data(self, variables, key):
       return variables.get(key)
